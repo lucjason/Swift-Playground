b030ce35b6d9a9ee2f3e429c196c55470814fee9
Jason Luc <lucjason+github@icloud.com>
2025-10-18 18:12:54 +0200

    Initial commit

---
+++ Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+let emoji = "😃"
+var greeting = "Hello, playground"
+
+print(greeting + " " + emoji)
+
+let x: Int = 10
+let y: Double = 20
+
+print("The sum of \(x) and \(y) is \(Double(x) + y)")
