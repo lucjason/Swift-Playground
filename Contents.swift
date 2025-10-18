12518686b4f878d70dee4ed622412f1f191da43d
Jason Luc <lucjason+github@icloud.com>
2025-10-18 18:24:16 +0200

    feat: convert a value

--- Contents.swift
+++ Contents.swift
@@ -9,3 +9,9 @@ let x: Int = 10
 let y: Double = 20
 
 print("The sum of \(x) and \(y) is \(Double(x) + y)")
+
+let label = "The width is: "
+let width = 44
+let widthDescription = label + String(width)
+
+print(widthDescription)
